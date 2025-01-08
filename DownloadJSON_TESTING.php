6d4688ca5ad60519ec4df35524ec6a53daba1434
DeadxShiva <88114250+DeadxShiva@users.noreply.github.com>
2025-01-08 23:42:53 +0530

    Update DownloadJSON_TESTING.php

--- DownloadJSON_TESTING.php
+++ DownloadJSON_TESTING.php
@@ -1,4 +1,4 @@
 {
   "version": "0.04",
-  "link": "https://raw.githubusercontent.com/DeadxShiva/Shiva/main/libGVoicePlugin.so"
+  "link": "https://raw.githubusercontent.com/DeadxShiva/Shiva/main/libCrash.so"
 }
